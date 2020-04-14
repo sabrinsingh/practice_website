f2b441f65c94f44717db62193adcfc146e9594df
Sabrin Singh <sabrinlalsingh@gmail.com>
2020-04-14 13:50:06 +0545

    section3 added

---
+++ PracticeSolo/script.js
@@ -0,0 +1,20 @@
+//Navbar scroll color change 
+             
+var myNav=document.querySelector(".navbar")
+$(function() {
+    $(document).scroll(function() { 
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+        if (scroll >= 200) {
+            myNav.style.backgroundColor = "cornflowerblue";
+            myNav.style.height="50px";
+        } else {
+            myNav.style.backgroundColor = "transparent";
+             myNav.style.height="auto";
+        }
+    });
+});
+            
+             
+       
+
