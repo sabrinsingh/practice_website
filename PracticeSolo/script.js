19400a0c86de9ba0d1ba2b5dbcaaf498223f66dc
Sabrin Singh <sabrinlalsingh@gmail.com>
2020-04-18 15:48:53 +0545

    Animation added

--- PracticeSolo/script.js
+++ PracticeSolo/script.js
@@ -1,19 +1,25 @@
 //Navbar scroll color change 
              
-var myNav=document.querySelector(".navbar")
+var myNav=document.querySelector(".navbar");
+console.log(myNav);
 $(function() {
     $(document).scroll(function() { 
     var scroll = $(window).scrollTop();
 //    console.log(scroll);
-        if (scroll >= 200) {
+        if (scroll >= 200 && window.matchMedia("(min-width: 991px)").matches) {
+            console.log("YES");
             myNav.style.backgroundColor = "cornflowerblue";
             myNav.style.height="50px";
         } else {
+            console.log("NO");
             myNav.style.backgroundColor = "transparent";
-             myNav.style.height="auto";
+            myNav.style.height="auto";
         }
+        console.log(myNav);
     });
 });
+
+
 
 
 
@@ -22,5 +28,12 @@ $(function() {
 //Testimonials Carousal Interval calculated in millisecond            
 $('.carousel').carousel({
   interval: 3000
-})       
+})   
+
+
+//Add Animtaion/ Initiate WOW JS
+$(function(){
+    'use strct';
+    new WOW().init();                  
+            });
 
